a4eca5d4d67553e6ba4102353d08aca772a292fe
Felix Urban <f.urban@urbicon.de>
2020-05-26 22:55:18 +0200

    Example for https://github.com/cypress-io/cypress/issues/7488

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
+describe("Route does not work after hash changed", function() {
+  before(function() {
     cy.visit('https://example.cypress.io')
-  })
-})
+  });
+  it("Replacing Location Hash leads to Mutliple Iframes", function() {
+    cy.window().then(win => {
+      win.location.replace('#test')
+    });
+  });
+});
